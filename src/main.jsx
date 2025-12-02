0b042a00c549d6e02c867b5e537a864db3bd6ed3
Andre Santos <asantos5@stevens.edu>
2025-12-02 18:36:48 -0500

    Error: Signup locked

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Removed <StrictMode> to prevent double-initialization race conditions with Firebase
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
